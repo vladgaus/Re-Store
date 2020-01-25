bfb5c324a3de8e73ee55d4b0adbd5378504dd722
Vladimir Sinica <bengalua@gmail.com>
2020-01-25 07:45:02 +0200

    Added action: UPDATE cart

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -6,6 +6,37 @@ const initialState = {
     orderTotal: 169
 };
 
+const updateCartPrices = (cartItems, item, idx) => {
+    if (idx === -1) {
+        return [
+            ...cartItems,
+            item
+        ];
+    }
+
+    return [
+        ...cartItems.slice(0, idx),
+        item,
+        ...cartItems.slice(idx + 1),
+    ];
+};
+
+const updateCartItem = (book, item = {}) => {
+
+    const {
+        id = book.id,
+        title = 'Book title',
+        count = 0,
+        total = 0} = item;
+
+    return {
+        id,
+        title,
+        count: count + 1,
+        total: total + book.price,
+    };
+};
+
 const reducer = (state = initialState, action) => {
 
     switch (action.type) {
@@ -33,19 +64,14 @@ const reducer = (state = initialState, action) => {
         case 'BOOK_ADDED_TO_CART':
             const bookId = action.payload;
             const book = state.books.find((book) => book.id === bookId);
-            const newItem = {
-                id: book.id,
-                name: book.title,
-                count: 1,
-                total: book.price,
-            };
+            const itemIndex = state.cartItems.findIndex(({id}) => id === bookId);
+            const item = state.cartItems[itemIndex];
+
+            const newItem = updateCartItem(book, item);
 
             return {
                 ...state,
-                cartItems: [
-                    ...state.cartItems,
-                    newItem
-                ],
+                cartItems: updateCartPrices(state.cartItems, newItem, itemIndex)
             };
 
         default:

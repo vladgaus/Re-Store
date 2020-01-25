d2c00b2097f7a509c48f094945f54ed833e6f972
Vladimir Sinica <bengalua@gmail.com>
2020-01-25 06:25:15 +0200

    Added shopping cart component

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,25 +1,43 @@
 const initialState = {
     books: [],
     loading: true,
-    error: null
+    error: null,
+    cartItems: [
+        {
+            id: 1,
+            name: 'Book 1',
+            count: 3,
+            total: 120
+        },
+        {
+            id: 2,
+            name: 'Book 2',
+            count: 1,
+            total: 49
+        },
+    ],
+    orderTotal: 169
 };
 
 const reducer = (state = initialState, action) => {
     switch (action.type) {
         case 'FETCH_BOOKS_REQUEST':
             return {
+                ...state,
                 books: [],
                 loading: true,
                 error: null
             };
         case 'FETCH_BOOKS_SUCCESS':
             return {
+                ...state,
                 books: action.payload,
                 loading: false,
                 error: null
             };
         case 'FETCH_BOOKS_FAILURE':
             return {
+                ...state,
                 books: [],
                 loading: false,
                 error: action.payload

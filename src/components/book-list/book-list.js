08b8cc13b8413063f63099b3eb528707dbdbf961
Vladimir Sinica <bengalua@gmail.com>
2020-01-25 18:01:50 +0200

    Clear reducer

--- src/components/book-list/book-list.js
+++ src/components/book-list/book-list.js
@@ -48,7 +48,7 @@ class BookListContainer extends Component {
     };
 }
 
-const mapStateToProps = ({books, loading, error}) => {
+const mapStateToProps = ({bookList : {books, loading, error}}) => {
     return {books, loading, error};
 };
 
